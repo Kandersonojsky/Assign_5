70e68a8af5439b11b792a6e1dfe9b81c5f3adc53
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-12-14 18:23:26 -0500

    Update run_them.sh

--- script_for/run_them.sh
+++ script_for/run_them.sh
@@ -1,11 +1,11 @@
 cd /scratch
 sudo mpicc -lm pi_mc.c -o pi_mc
-sudo chmod 777 np2.sh
-sudo chmod 777 np4.sh
-sudo chmod 777 np6.sh
-sudo chmod 777 np8.sh
-sudo chmod 777 np10.sh
-sudo chmod 777 np12.sh
+sudo chmod 777 /local/repository/script_for/np2.sh
+sudo chmod 777 /local/repository/script_for/np4.sh
+sudo chmod 777 /local/repository/script_for/np6.sh
+sudo chmod 777 /local/repository/script_for/np8.sh
+sudo chmod 777 /local/repository/script_for/np10.sh
+sudo chmod 777 /local/repository/script_for/np12.sh
 sudo sbatch -N 2 np2.sh
 sudo sbatch -N 4 np4.sh
 sudo sbatch -N 6 np6.sh
