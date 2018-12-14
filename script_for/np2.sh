96de10f350c5bc915bede526437bc61c80f57811
Kandersonojsky <43188988+Kandersonojsky@users.noreply.github.com>
2018-12-14 18:11:33 -0500

    Update np2.sh

--- script_for/np2.sh
+++ script_for/np2.sh
@@ -3,4 +3,4 @@ set -x
 echo $SLURM_SUBMIT_DIR
 cd /scratch
 echo "With 2 processes"
-time mpirun -np 2 --mca btl_base_warn_component_unused 0 pi_mc
+time mpirun -np 2 --mca btl_base_warn_component_unused 0 pi_mc 10000000
